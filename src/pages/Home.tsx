c90a14f760dbc2b40bdd41458e25ad7d352d591e
Aryap <veloxstudios@outlook.com>
2026-03-02 18:19:47 -0500

    Fix homepage order: Hero > Video > Awards > Clients > Services > Contact, remove Team from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,18 @@
 import { Hero } from '../components/Hero'
 import { VideoSection } from '../components/VideoSection'
-import { Clients } from '../components/Clients'
 import { Awards } from '../components/Awards'
+import { Clients } from '../components/Clients'
 import { Services } from '../components/Services'
-import { Team } from '../components/Team'
 import { Contact } from '../components/Contact'
 
 export function Home() {
     return (
         <>
             <Hero />
-            <Clients />
-            <Awards />
             <VideoSection />
+            <Awards />
+            <Clients />
             <Services />
-            <Team />
             <Contact />
         </>
     )
